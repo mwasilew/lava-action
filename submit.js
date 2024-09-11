0fdc9a13daa706d0b3516470cb640c191986a736
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2024-09-11 10:37:13 +0100

    Support job log streaming
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- submit.js
+++ submit.js
@@ -1,51 +1,109 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-//const job_definition_path = core.getInput('job-definition')
-//const lava_token = core.getInput('lava-token')
-//const lava_url = core.getInput('lava-url')
-
 const job_definition_path = core.getInput('job_definition')
 const lava_token = core.getInput('lava_token')
 const lava_url = core.getInput('lava_url')
+const wait_for_job = core.getInput('wait_for_job')
+
+// colors
+
+const ColorReset = "\x1b[0m";
+
+enum BackgroudColor {
+    info = "\x1b[46m",
+    debug = "\x1b[43m",
+    results = "\x1b[44m",
+    target = "\x1b[42m",
+    error = "\x1b[41m",
+    exception = "\x1b[45m",
+}
+
+
 // read from file
 const fs = require ('fs');
-const file = fs.readFileSync(job_definition_path, 'utf-8');
-
+const file;
+try {
+  file = fs.readFileSync(job_definition_path, 'utf-8');
+} catch (err) {
+  console.log("Error reading job definition file")
+  core.setFailed(err.message);
+}
 // submit to lava
-const https = require('https');
+const request = require('request');
 
-var postData = JSON.stringify({
-    'definition' : file
-});
+var lava_job_ids;
+
+function callback(error, response, body) {
+  if (error) {
+      console.log(error)
+      core.setFailed(error)
+  }
+  if (response.statusCode != 201) {
+      core.setFailed(response.statusCode)
+  }
+  var res = JSON.parse(body);
+  lava_jobs_ids = res.job_ids
+}
 
 const tokenString = 'Token ' + lava_token
 
 var options = {
-  hostname: lava_url,
-  port: 443,
-  path: '/api/v0.2/jobs/',
+  hostname: 'https://' + lava_url + '/api/v0.2/jobs/',
   method: 'POST',
   headers: {
-       'Content-Type': 'application/json',
-       'Content-Length': postData.length,
        'Authorization': tokenString
+     },
+  json: {
+       'definition' : file
      }
 };
 
-var req = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+request(options, callback)
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-});
+if ( wait_for_job ) {
+  for ( const job_id in lava_job_ids ) {
+    // check job status
+    var job_status;
+    var job_status_options = {
+      hostname: 'https://' + lava_url + '/api/v0.2/jobs/' + job_id + '/',
+      method: 'GET',
+      headers: {
+           'Authorization': tokenString
+         }
+    };
 
-req.on('error', (e) => {
-  console.error(e);
-});
+    function job_status_callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        const job = JSON.parse(body);
+        job_status = job.State
+      }
+    }
+    request(job_status_options, job_status_callback)
 
-req.write(postData);
-req.end();
+    var log_start = 0;
+    while ( job_status == "Running" ) {
+      // stream job logs
+      var job_log_options = {
+        hostname: 'https://' + lava_url + '/api/v0.2/jobs/' + job_id + '/logs/?start=' + log_start,
+        method: 'GET',
+        headers: {
+          'Authorization': tokenString
+          }
+      };
 
+      function job_log_callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          const job_log = JSON.parse(body);
+          job_log.forEach(function(line) {
+            var textFormat = BackgroundColor[line.lvl as keyof typeof BackgroundColor]
+            console.log(`${textFormat}${line.msg}${ColorReset}`);
+            log_start += 1;
+          });
+        }
+      }
+      request(job_log_options, job_log_callback)
+      request(job_status_options, job_status_callback)
+    }
+  }
+}
